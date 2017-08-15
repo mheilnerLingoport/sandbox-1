19f0c9d50cc7333d784bc4864c5c8235624cf3ef
Olivier Libouban <oliboubanLingoport@users.noreply.github.com>
2017-08-15 15:08:58 -0600

    Added doubles, NON-NLS-L for logger

--- java/lingoport/com/lingoport/HelloWorld.java
+++ java/lingoport/com/lingoport/HelloWorld.java
@@ -8,8 +8,7 @@ import java.util.logging.Logger;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		String greeting = "Hello";
-		greeting += " World";
+		String greeting = "Hello World";
 		String goodbye = " Goodbye World";
 
 		greeting.equalsIgnoreCase(goodbye);
@@ -17,14 +16,14 @@ public class HelloWorld {
 		DateFormat dateFormatter = DateFormat.getDateInstance(DateFormat.DEFAULT);
 		String dateString = dateFormatter.format(new Date());
 
-		Double inputNum = new Double("2.57");
+		Double inputNum = new Double("2.57") + new Double("3.45");
 
 		System.out.println(greeting);
 		System.out.println(inputNum);
 		System.out.println(dateString);
 
 		Logger logger = Logger.getLogger("Logger");
-		String logMessage = "Logging message - Don't translate me.";
+		String logMessage = "Logging message - Don't translate me."; // $NON-NLS-L$
 		logger.log(Level.INFO, logMessage);
 	}
 }
